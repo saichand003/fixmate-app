682fb2be95e4ab4a7e388813311a8f753836d22d
saichand003 <saichand0438@gmail.com>
2025-07-15 23:53:39 -0500

    updating the files

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,8 +1,16 @@
 /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   experimental: {
+//     turbo: false //turn off Turbopack
+//   }
+// };
+
+// module.exports = nextConfig;
+
+// next.config.js
 const nextConfig = {
-  experimental: {
-    turbo: false //turn off Turbopack
-  }
+  reactStrictMode: true,
+  // other config here, but no "experimental.turbo"
 };
 
 module.exports = nextConfig;
